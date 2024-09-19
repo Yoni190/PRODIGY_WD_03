787a5a18bd414f3f1d148848f1d4e8338d684aca
Yoni190 <yfisuhu@gmail.com>
2024-09-19 12:41:33 +0300

    Implement game logic

--- script.js
+++ script.js
@@ -3,9 +3,41 @@ let board = document.querySelector(".board");
 let first = document.querySelector(".first");
 let second = document.querySelector(".second");
 let third = document.querySelector(".third");
+let scoreBoard = document.querySelector(".score")
 let array = [];
+let score1 = 0;
+let score2 = 0;
+let player1 = null;
+let player2 = null;
 
 let isPlayer1 = true;
+
+function getNames(){
+    player1 = prompt("Player1's name: ")
+    player2 = prompt("Player2's name: ")
+
+    addPlayers(player1, player2);
+}
+
+function addPlayers(player1, player2){
+    const p1 = document.createTextNode(`${player1} : ${score1}`);
+    const p2 = document.createTextNode(`${player2} : ${score2}`);
+
+    scoreBoard.appendChild(p1);
+    scoreBoard.appendChild(document.createElement("br"));
+    scoreBoard.appendChild(p2);
+}
+
+function incrementScore(){
+    scoreBoard.textContent = "";
+    array = [];
+    const p1 = document.createTextNode(`${player1} : ${score1}`);
+    const p2 = document.createTextNode(`${player2} : ${score2}`);
+
+    scoreBoard.appendChild(p1);
+    scoreBoard.appendChild(document.createElement("br"));
+    scoreBoard.appendChild(p2);
+}
 
 function createBlocks() {
     size = 9;
@@ -16,6 +48,7 @@ function createBlocks() {
         block.style.height = "100px";
         block.style.width = "100px";
         block.style.fontSize = "90px";
+        block.style.textAlign = "center";
         if (i > 5){
             third.appendChild(block);
         }
@@ -26,29 +59,75 @@ function createBlocks() {
             first.appendChild(block);
         }
         array.push(block);
+        block.onclick = () => chooseSquare(block);
+        
     }
 }
 
-blocks.forEach((block) => {
-    block.addEventListener("click", () => {
-        if (isPlayer1 && block.textContent == ""){
-            block.textContent = "X";
-            isPlayer1 = false;
-        }
-        else if(!isPlayer1 && block.textContent == "") {
-            block.textContent = "O";
-            isPlayer1 = true;
-        }
-        if (
-            (one.textContent === "X" && two.textContent === "X" && three.textContent === "X") ||
-            (one.textContent === "O" && two.textContent === "O" && three.textContent === "O")
-        ) {
-                alert("You won!");
-            }
-            
-        
-        
-    });
-})
+function clearBoard(){
+    first.textContent = ""
+    second.textContent = ""
+    third.textContent = ""
+}
 
+function chooseSquare(block){
+    if (isPlayer1){
+        block.textContent = "X";
+        isPlayer1 = false;
+    }
+    else if(!isPlayer1 && block.textContent == "") {
+        block.textContent = "O";
+        isPlayer1 = true;
+    }
+    if (isWin()) {
+        alert("You won!")
+    }
+    
+}
+
+function isWin(){
+    if (
+        array[0].textContent == "O" && array[1].textContent == "O" && array[2].textContent == "O" ||
+        array[3].textContent == "O" && array[4].textContent == "O" && array[5].textContent == "O" ||
+
+        array[6].textContent == "O" && array[7].textContent == "O" && array[8].textContent == "O" ||
+        array[0].textContent == "O" && array[3].textContent == "O" && array[6].textContent == "O" ||
+         
+        array[1].textContent == "O" && array[4].textContent == "O" && array[7].textContent == "O" ||
+        array[2].textContent == "O" && array[5].textContent == "O" && array[8].textContent == "O" ||
+         
+        array[0].textContent == "O" && array[4].textContent == "O" && array[8].textContent == "O" ||
+        array[2].textContent == "O" && array[4].textContent == "O" && array[6].textContent == "O" 
+        
+    ){
+        score2++;
+        incrementScore();
+        clearBoard();
+        createBlocks();
+        return true; 
+        
+    }
+    else if (array[0].textContent == "X" && array[1].textContent == "X" && array[2].textContent == "X" ||
+             array[3].textContent == "X" && array[4].textContent == "X" && array[5].textContent == "X" ||
+
+             array[6].textContent == "X" && array[7].textContent == "X" && array[8].textContent == "X" ||
+             array[0].textContent == "X" && array[3].textContent == "X" && array[6].textContent == "X" ||
+
+             array[1].textContent == "X" && array[4].textContent == "X" && array[7].textContent == "X" ||
+             array[2].textContent == "X" && array[5].textContent == "X" && array[8].textContent == "X" ||
+
+             array[0].textContent == "X" && array[4].textContent == "X" && array[8].textContent == "X" ||
+             array[2].textContent == "X" && array[4].textContent == "X" && array[6].textContent == "X" 
+        ) {
+        score1++;
+        incrementScore();
+        clearBoard();
+        createBlocks();
+        return true;
+        
+        }
+
+}
+
+getNames();
 createBlocks();
