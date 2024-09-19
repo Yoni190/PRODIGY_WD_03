7584ef62cd90313e0a425c6450c93fd0fdaa0965
Yoni190 <yfisuhu@gmail.com>
2024-09-19 09:42:36 +0300

    Populate board using JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 let blocks = document.querySelectorAll(".block");
+let board = document.querySelector(".board");
+let first = document.querySelector(".first");
+let second = document.querySelector(".second");
+let third = document.querySelector(".third");
+let array = [];
+
 let isPlayer1 = true;
+
+function createBlocks() {
+    size = 9;
+    for(let i = 0; i < size; i++){
+        const block = document.createElement("div");
+        block.className = "block";
+        block.style.border = "2px solid black";
+        block.style.height = "100px";
+        block.style.width = "100px";
+        block.style.fontSize = "90px";
+        if (i > 5){
+            third.appendChild(block);
+        }
+        else if (i > 2){
+            second.appendChild(block);
+        }
+        else {
+            first.appendChild(block);
+        }
+        array.push(block);
+    }
+}
 
 blocks.forEach((block) => {
     block.addEventListener("click", () => {
@@ -11,6 +39,16 @@ blocks.forEach((block) => {
             block.textContent = "O";
             isPlayer1 = true;
         }
+        if (
+            (one.textContent === "X" && two.textContent === "X" && three.textContent === "X") ||
+            (one.textContent === "O" && two.textContent === "O" && three.textContent === "O")
+        ) {
+                alert("You won!");
+            }
+            
+        
         
     });
 })
+
+createBlocks();
